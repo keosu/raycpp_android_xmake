d29d834cf6bef481f6266f567899c437392f7c6b
Yi Jianlong <jianlong.yi@outlook.com>
2025-12-16 00:08:05 +0800

    Update

--- xmake.lua
+++ xmake.lua
@@ -11,16 +11,13 @@ target("raydemo_android")
     add_files("src/main.cpp") 
     add_packages("raylib")
     
-    if is_plat("android") then  
- 
-        add_rules("android.native_app", {
-            android_sdk_version = "35",
-            android_manifest = "android/AndroidManifest.xml",
-            android_res = "android/res",
-            keystore = "android/debug.jks",
-            keystore_pass = "123456",
-            package_name = "com.raylib.demo"
-        })
-    end
+    add_rules("android.native_app", {
+        android_sdk_version = "35",
+        android_manifest = "android/AndroidManifest.xml",
+        android_res = "android/res",
+        keystore = "android/debug.jks",
+        keystore_pass = "123456",
+        package_name = "com.raylib.demo"
+    })
 
  
